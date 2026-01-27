c062fcedce563841c88a6c2fce3d26f25cb713ae
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:23:08 -0300

    feat: ApplyMigrationsAndSeedAsync

---
+++ Devices.Api/Extensions/HostExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Devices.Application.Interfaces;
+using Devices.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devices.Api.Extensions
+{
+    public static class HostExtensions
+    {
+        public static async Task ApplyMigrationsAndSeedAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var db = scope.ServiceProvider.GetRequiredService<DeviceDbContext>();
+            await db.Database.MigrateAsync();
+
+            var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+            await seeder.SeedAsync(CancellationToken.None);
+        }
+    }
+}
