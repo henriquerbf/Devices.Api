603e3d6d604b38cb0902210968ae53a56d9aebe5
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:04:15 -0300

    Create DependencyInjection.cs

---
+++ Devices.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using Devices.Application.Interfaces;
+using Devices.Application.Interfaces.Repositories;
+using Devices.Infrastructure.Persistance;
+using Devices.Infrastructure.Repositories;
+using Devices.Infrastructure.Seeding;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Devices.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString =
+                configuration.GetConnectionString("DevicesDb")
+                ?? throw new InvalidOperationException(
+                    "Connection string 'DevicesDb' not found.");
+
+            services.AddDbContext<DeviceDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sql =>
+                {
+                    sql.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(10),
+                        errorNumbersToAdd: null);
+                });
+            });
+
+            // Repositories (Outbound Adapters)
+            services.AddScoped<IDeviceRepository, DeviceRepository>();
+
+            // Seeding
+            services.AddScoped<IDataSeeder, DeviceDataSeeder>();
+
+            return services;
+        }
+    }
+}
