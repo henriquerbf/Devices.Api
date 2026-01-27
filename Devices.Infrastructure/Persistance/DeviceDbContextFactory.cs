0470981f10599c662a4f1263a14f65ece608248d
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:04:04 -0300

    feat: create DeviceDbContextFactory
    to do migrations without need to run application

---
+++ Devices.Infrastructure/Persistance/DeviceDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Devices.Infrastructure.Persistance
+{
+    public sealed class DeviceDbContextFactory : IDesignTimeDbContextFactory<DeviceDbContext>
+    {
+        public DeviceDbContext CreateDbContext(string[] args)
+        {
+            // Used only by EF tooling (dotnet ef). Keep it minimal.
+            var cs =
+                "Server=localhost,1433;" +
+                "Database=DevicesDb;" +
+                "User Id=sa;" +
+                "Password=teste@123;" +
+                "TrustServerCertificate=True;" +
+                "Encrypt=False;";
+
+            var options = new DbContextOptionsBuilder<DeviceDbContext>()
+                .UseSqlServer(cs)
+                .Options;
+
+            return new DeviceDbContext(options);
+        }
+    }
+}
