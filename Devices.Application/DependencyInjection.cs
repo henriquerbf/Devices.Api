5a2b1b5a331d0dfb9d1f7b4266cfba85a0223836
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:18:38 -0300

    feat: Create DependencyInjection

---
+++ Devices.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Devices.Application.UseCases.Devices;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devices.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<CreateDeviceUseCase>();
+            services.AddScoped<GetDeviceByIdUseCase>();
+            services.AddScoped<GetDevicesUseCase>();
+            services.AddScoped<GetDevicesByBrandUseCase>();
+            services.AddScoped<GetDevicesByStateUseCase>();
+            services.AddScoped<UpdateDeviceUseCase>();
+            services.AddScoped<PatchDeviceUseCase>();
+            services.AddScoped<DeleteDeviceUseCase>();
+
+            return services;
+        }
+    }
+}
