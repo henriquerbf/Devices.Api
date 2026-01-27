c5c0ec817a6a32152c07b5a9d70d5719f098c147
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 02:48:46 -0300

    feat: adding error handlers, migrations and data seed

--- Devices.Api/Program.cs
+++ Devices.Api/Program.cs
@@ -1,15 +1,28 @@
+using Devices.Api.Extensions;
+using Devices.Api.Middleware;
+using Devices.Application;
+using Devices.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Hexagonal wiring
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// Global exception handling
+builder.Services.AddTransient<ExceptionHandlingMiddleware>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -22,4 +35,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Apply migrations + seed (startup)
+await app.ApplyMigrationsAndSeedAsync();
+
 app.Run();
+
+public partial class Program { } // useful for integration tests
