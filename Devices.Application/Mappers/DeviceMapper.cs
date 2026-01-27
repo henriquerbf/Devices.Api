960fb0f19e0b71923a518711505bcfc5971cf711
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:31:51 -0300

    Create DeviceMapper.cs

---
+++ Devices.Application/Mappers/DeviceMapper.cs
@@ -0,0 +1,17 @@
+﻿using Devices.Application.Contracts.Devices;
+using Devices.Domain.Entities;
+
+namespace Devices.Application.Mappers
+{
+    public static class DeviceMapper
+    {
+        public static DeviceResponse ToResponse(Device device)
+            => new(
+                device.Id,
+                device.Name,
+                device.Brand,
+                device.State,
+                device.CreationTime
+            );
+    }
+}
