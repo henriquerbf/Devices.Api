3a771e71babc6182ac96a8ae167ebc33ac252356
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:00:22 -0300

    feat: create device configuration
    it validates the values related to what database is expecting

---
+++ Devices.Infrastructure/Persistance/Configurations/DeviceConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Devices.Domain.Entities;
+using Devices.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Devices.Infrastructure.Persistance.Configurations
+{
+    public sealed class DeviceConfiguration : IEntityTypeConfiguration<Device>
+    {
+        public void Configure(EntityTypeBuilder<Device> builder)
+        {
+            builder.ToTable("devices");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Brand)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.State)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => Enum.Parse<DeviceState>(v, ignoreCase: true))
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(x => x.CreationTime)
+                .IsRequired();
+
+            builder.HasIndex(x => x.Brand);
+            builder.HasIndex(x => x.State);
+        }
+    }
+}
