b380d594cf52520d5faffe82af7785f3a9349d14
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:32:00 -0300

    Create NotFoundException.cs

---
+++ Devices.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devices.Application.Exceptions
+{
+    public sealed class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
