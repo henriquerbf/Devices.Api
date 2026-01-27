19b4b575fc76a12f19ffc62f3445da8869a9d33d
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:31:48 -0300

    Create IDeviceRepository.cs

---
+++ Devices.Application/Interfaces/Repositories/IDeviceRepository.cs
@@ -0,0 +1,20 @@
+﻿using Devices.Domain.Entities;
+using Devices.Domain.Enums;
+
+namespace Devices.Application.Interfaces.Repositories
+{
+    public interface IDeviceRepository
+    {
+        Task<Device?> GetByIdAsync(Guid id, CancellationToken ct);
+        Task<IReadOnlyList<Device>> GetAllAsync(CancellationToken ct);
+        Task<IReadOnlyList<Device>> GetByBrandAsync(string brand, CancellationToken ct);
+        Task<IReadOnlyList<Device>> GetByStateAsync(DeviceState state, CancellationToken ct);
+
+        Task AddAsync(Device device, CancellationToken ct);
+        void Update(Device device);
+        void Remove(Device device);
+
+        Task<int> SaveChangesAsync(CancellationToken ct);
+    }
+
+}
