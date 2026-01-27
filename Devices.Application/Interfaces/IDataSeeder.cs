a3e1b85ab5e70e9ca7a62c260e42e23bf0df5365
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 03:31:31 -0300

    Create IDataSeeder.cs

---
+++ Devices.Application/Interfaces/IDataSeeder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devices.Application.Interfaces
+{
+    public interface IDataSeeder
+    {
+        Task SeedAsync(CancellationToken cancellationToken);
+    }
+}
