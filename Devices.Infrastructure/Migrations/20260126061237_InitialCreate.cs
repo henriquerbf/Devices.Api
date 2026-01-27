8514cefc022d0a385f37fdc559e2c6cdca0297a6
Henrique Faria <henriquerbf@hotmail.com>
2026-01-27 02:39:18 -0300

    chore: adding migrations

---
+++ Devices.Infrastructure/Migrations/20260126061237_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Devices.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    CreationTime = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_devices_Brand",
+                table: "devices",
+                column: "Brand");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_devices_State",
+                table: "devices",
+                column: "State");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "devices");
+        }
+    }
+}
